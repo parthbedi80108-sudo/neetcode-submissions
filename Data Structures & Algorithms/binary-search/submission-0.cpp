2e4b044933bde3d692f6f7c798b8660e7bbb19a8
Parth Bedi <parthbedi80108@gmail.com>
2026-07-19 17:04:48 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0;
+        int r=nums.size()-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(target<nums[mid])r=mid-1;
+            else if(nums[mid]==target)return mid;
+            else l=mid+1;
+        }
+        return -1;
+    }
+};
