c38b7a234c3480a7ce484f8c62faaa1096e956ff
Parth Bedi <parthbedi80108@gmail.com>
2026-07-22 21:21:01 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1||n==2)return n;
+        vector<int>DP(n+1);
+        DP[1]=1;
+        DP[2]=2;
+        for(int i=3;i<=n;i++)DP[i]=DP[i-1]+DP[i-2];
+        return DP[n];
+    }
+};
